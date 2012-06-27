fb88f3bc3c2294c1e8f98d13278897d515d4b025
Mark Hatherly <mwhatherly@alumni.grinnell.edu>
2012-06-26 20:35:33 -0500

    Add contact static page 3.5.1

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -56,5 +56,20 @@ describe "StaticPages" do
 	end
     
   end
- 
+ describe "Contact page" do
+	it "should have the content 'Contact'" do
+     visit '/static_pages/contact'
+      page.should have_content('Contact')
+    end
+	it "should have the h1 'Contact'" do
+		visit '/static_pages/contact'
+		page.should have_selector('h1', :text => 'Contact')
+	end
+	it "should have the title  'About Us'" do
+		visit '/static_pages/contact'
+		page.should have_selector('title', 
+			:text => "Ruby on Rails Tutorial Sample App | Contact")
+	end
+    
+  end
 end
