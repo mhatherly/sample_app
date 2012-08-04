d6b1aea9fb83b9505326f0d1e5197dce7ebba3a5
Mark Hatherly <mwhatherly@alumni.grinnell.edu>
2012-08-04 13:20:12 -0500

    add tests for successful user create result

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,6 +29,8 @@ class User < ActiveRecord::Base
    validates :email, presence: true, format: {with: VALID_EMAIL_REGEX },
 					 uniqueness: { case_sensitive: false }
 					 
-   validates  :password, presence: true, length: { minimum: 6 }
-   validates  :password_confirmation, presence: true					
+#   presence removed to fix error message - now handled in UI: exercise 7.6.3
+	validates  :password, length: { minimum: 6 }
+
+   validates  :password_confirmation, presence: true
  end
