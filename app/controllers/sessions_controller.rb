2ffc44bff4654165b675d6315877de11d5b81fff
Mark Hatherly <mwhatherly@alumni.grinnell.edu>
2012-08-11 13:59:24 -0500

    changed login to use form_tag instead of form_for ex 8.1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,8 @@ class SessionsController < ApplicationController
    end
    
    def create 
-      user =  User.find_by_email(params[:session][:email])
-      if user && user.authenticate(params[:session][:password]) 
+      user =  User.find_by_email(params[:email])
+      if user && user.authenticate(params[:password]) 
          #sign in the user and redirect to the user show page
          sign_in user
          redirect_to user
