f26de87e113a790707b32af03c5db5f265d31391
Mark Hatherly <mwhatherly@alumni.grinnell.edu>
2012-08-12 16:20:51 -0500

    added user edit form and first level of authorization (9.1 9.2.1)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:edit, :update]
+  
   def show
 	@user = User.find(params[:id])
   end
@@ -18,4 +20,26 @@ class UsersController < ApplicationController
 		render 'new'
 	end
   end 
+
+  def edit
+     @user = User.find(params[:id])
+  end
+  
+  def update 
+     @user=User.find(params[:id])
+     if @user.update_attributes(params[:user]) 
+        flash[:success] = "Profile updated"
+        sign_in @user
+        redirect_to @user
+     else
+		render 'edit'
+	 end
+  end
+  
+  private
+  
+  def signed_in_user
+     redirect_to signin_path, notice: "Please sign in." unless signed_in?
+  end
+
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -9,4 +9,12 @@ def valid_signin(user)
 	click_button "Sign in"
 end
 
+def sign_in(user)
+	visit signin_path
+	fill_in "Email",  with: user.email
+	fill_in "Password", with: user.password
+	click_button "Sign in"
+	# Sign in when not using Capybara as well
+	cookies[:remember_toke] = user.remember_token
+end
 
