f30b3b29ed6e83125501b9c8e77ece74244523d7
Mark Hatherly <mwhatherly@alumni.grinnell.edu>
2012-08-16 17:52:07 -0500

    add administrator attribute to the user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,7 @@
 #  updated_at      :datetime        not null
 #  password_digest :string(255)
 #  remember_token  :string(255)
+#  admin           :boolean         default(FALSE)
 #
 
 require 'spec_helper'
@@ -25,9 +26,11 @@ describe User do
 	it { should respond_to(:password) }
 	it { should respond_to(:password_confirmation) }
 	it { should respond_to(:remember_token) }
+	it { should respond_to(:admin) }
 	it { should respond_to(:authenticate) }
 	
 	it { should be_valid }
+	it { should_not be_admin }
 	
 	describe "when name is not present" do
 		before { @user.name = " " }
@@ -131,6 +134,12 @@ describe User do
 		before { @user.password = @user.password_confirmation = "a"*5 }
         it {should be_invalid}
     end
+    describe "with admin attribute set to 'true' " do
+		before { @user.toggle!(:admin) }
+		
+		it { should be_admin }
+    end
+    
     describe "remember token" do
        before { @user.save } 
        its(:remember_token) { should_not be_blank }
