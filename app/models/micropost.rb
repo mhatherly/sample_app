918982fea411bae9e29bb196e987807d128f10f8
Mark Hatherly <mwhatherly@alumni.grinnell.edu>
2012-08-19 21:26:34 -0500

    add default ordering to microposts 10.1.4a

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,6 +3,8 @@ class Micropost < ActiveRecord::Base
   belongs_to :user
   
   validates :user_id,  presence: true
+  
+  default_scope order: 'microposts.created_at DESC'
 end
 # == Schema Information
 #
