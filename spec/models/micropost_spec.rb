89f3decf347c7b020db9e16a3272d7dc2743c242
Mark Hatherly <mwhatherly@alumni.grinnell.edu>
2012-08-20 20:06:17 -0500

    add mposts content validation  10.1.5

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -29,6 +29,19 @@ describe Micropost do
        end.should raise_error(ActiveModel::MassAssignmentSecurity::Error)
      end  # should not allow
    end # accessible attributes
+ 
+   describe "with blank content" do
+      before { @micropost.content = " " } 
+      it { should_not be_valid }
+   end
+   
+   describe "with content that is too long " do
+       before { @micropost.content = "a" * 141  }
+       it { should_not be_valid }
+   end
+ 
+ 
+ 
  end
 # == Schema Information
 #
