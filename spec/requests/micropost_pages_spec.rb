f2e390cbedc9a7a27152a97a0db2551e9abf53c6
Mark Hatherly <mwhatherly@alumni.grinnell.edu>
2012-09-04 17:35:05 -0500

    deleting mpost  10.3.4

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -30,4 +30,16 @@ describe "Micropost pages" do
       end  
     end # with valid information 
   end #micropost creation 
+  describe "Micropost destruction" do
+    before { FactoryGirl.create(:micropost, user: user) }
+    
+    describe "as correct user" do
+      before {visit root_path} 
+      
+      it "should delete a micropost" do
+        expect { click_link "delete" }.should change(Micropost, :count).by(-1)
+      end # should delete a micropost 
+    end # as correct user    
+  end # Micropost destruction
+  
 end # Micropost pages
