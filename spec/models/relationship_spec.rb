69bc63562fb980ac552060f78721f57046c5a3ae
Mark Hatherly <mwhatherly@alumni.grinnell.edu>
2012-09-12 19:59:43 -0500

    add relationship model basic validations 11.1.3

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -24,6 +24,16 @@ describe Relationship do
    its(:followed) { should == followed }
   end
 
+  describe "when followed id is not present"  do
+    before { relationship.followed_id = nil }
+    it { should_not be_valid }
+   end
+  
+  describe "when follower id is not present"  do
+    before { relationship.follower_id = nil }
+    it { should_not be_valid }
+   end
+
 end
 # == Schema Information
 #
